d262d027e39a763eb4cd40acc3004a5569dcae4e
richard <richardzhouca@gmail.com>
2015-01-24 21:40:10 -0800

    add check

--- aws_script/recipes/default.rb
+++ aws_script/recipes/default.rb
@@ -1,7 +1,6 @@
 execute "install aws script" do
   command "sudo curl https://raw.githubusercontent.com/timkay/aws/master/aws -o /usr/bin/aws"
   not_if "test -e /usr/bin/aws"  
-  action :nothing
 end
 
 
